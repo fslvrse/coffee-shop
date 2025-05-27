313b13c48d77e224351126875bf466848d0fad01
faisal khasim <fslvrse2004@gmail.com>
2025-05-27 22:35:38 +0300

    Update order.py

--- order.py
+++ order.py
@@ -1,33 +1,43 @@
 class Order:
-    _all = []
-
     def __init__(self, customer, coffee, price):
-        from customer import Customer
-        from coffee import Coffee
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
 
-        if not isinstance(customer, Customer):
-            raise ValueError("Customer must be an instance of Customer.")
-        if not isinstance(coffee, Coffee):
-            raise ValueError("Coffee must be an instance of Coffee.")
-        if not isinstance(price, float) or not (1.0 <= price <= 10.0):
-            raise ValueError("Price must be a float between 1.0 and 10.0.")
-
-        self._customer = customer
-        self._coffee = coffee
-        self._price = price
-
-        Order._all.append(self)
-        customer._orders.append(self)
-        coffee._orders.append(self)
+        customer.add_order(self)
+        coffee.add_order(self)
 
     @property
     def customer(self):
         return self._customer
 
+    @customer.setter
+    def customer(self, value):
+        from customer import Customer
+        if not isinstance(value, Customer):
+            raise ValueError("Customer must be an instance of Customer.")
+        self._customer = value
+
     @property
     def coffee(self):
         return self._coffee
 
+    @coffee.setter
+    def coffee(self, value):
+        from coffee import Coffee
+        if not isinstance(value, Coffee):
+            raise ValueError("Coffee must be an instance of Coffee.")
+        self._coffee = value
+
     @property
     def price(self):
         return self._price
+
+    @price.setter
+    def price(self, value):
+        if hasattr(self, "_price"):
+            raise AttributeError("Price cannot be changed after initialization.")
+        if isinstance(value, float) and 1.0 <= value <= 10.0:
+            self._price = value
+        else:
+            raise ValueError("Price must be a float between 1.0 and 10.0.")
